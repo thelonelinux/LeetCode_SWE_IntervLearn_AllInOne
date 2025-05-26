ec3b5b128e91bd009c78b098a80ce5f7206a9d44
thelonelinux <vickythetechguy@gmail.com>
2025-05-27 02:19:41 +0530

    codeForce contest 2110 practise

---
+++ src/Codeforces_Contest/CodeforceContest2110.java
@@ -0,0 +1,19 @@
+package Codeforces_Contest;
+
+import java.util.Scanner;
+
+public class CodeforceContest2110 {
+
+     public static void main(String[] args) {
+
+        
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+ 
+        while(t-->0){
+            
+        }
+
+     }
+    
+}
