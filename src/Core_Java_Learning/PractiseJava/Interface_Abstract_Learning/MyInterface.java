0ddb99b094bc088f4e68aa3d415a772199794e39
Vicky Prasad <vicky@Vickys-MacBook-Air.local>
2026-06-09 01:30:04 +0530

    Java Updates

--- src/Core_Java_Learning/PractiseJava/Interface_Abstract_Learning/MyInterface.java
+++ src/Core_Java_Learning/PractiseJava/Interface_Abstract_Learning/MyInterface.java
@@ -20,14 +20,21 @@ public interface MyInterface {
     /**
      * INTERFACE METHODS
      * By default all the methods are Abstract and Public. No need to specifically denote with those keywords.
-     * However, default keyword can be used to add some body in the methods in interface.
+     * However, default keyword can be used to add some body in the methods in interface. 
+     * They are not abstract, as they have body, but they are public by default. They can be overridden in the implementing class, but it is not mandatory to override as it has body, so it's not abstract.
      * Static method too can have body like any other normal class, because it's class properties so
      *
+     * A default method in Java is a method declared inside an interface with the default keyword and a method body.
+     * It was introduced in Java 8 to allow interfaces to add new methods without breaking existing classes that implement them.
      */
 
     void normalInterfaceMethodPublicAndAbstract(); //If you add body, it will give compilation error
 
-    default void implementDefaultMethod(){
+    default void implementDefaultMethod(){ //This method can be overridden in the implementing class, but it is not mandatory to override as it has body, so it's not abstract.
+        System.out.println("Default method in interface have body, they are not like abstract method");
+    }
+
+    default void implementDefaultMethod2(){ //This method can be overridden in the implementing class, but it is not mandatory to override as it has body, so it's not abstract.
         System.out.println("Default method in interface have body, they are not like abstract method");
     }
 
